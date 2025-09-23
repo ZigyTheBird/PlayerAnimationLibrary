21204d3a666e16036d86612d639b93f0ca9baeb8
dima_dencep <dima_dencep@redlance.org>
2025-09-24 00:07:41 +0700

    Update to fabric resource loader v1

--- minecraft/fabric/src/main/java/com/zigythebird/playeranim/fabric/PlayerAnimLibModFabric.java
+++ minecraft/fabric/src/main/java/com/zigythebird/playeranim/fabric/PlayerAnimLibModFabric.java
@@ -1,18 +1,18 @@
 package com.zigythebird.playeranim.fabric;
 
 import com.zigythebird.playeranim.PlayerAnimLibMod;
+import com.zigythebird.playeranim.animation.PlayerAnimResources;
 import com.zigythebird.playeranim.commands.PlayerAnimCommands;
-import com.zigythebird.playeranim.fabric.client.IdentifiablePlayerAnimResources;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.packs.PackType;
 
 public final class PlayerAnimLibModFabric extends PlayerAnimLibMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new IdentifiablePlayerAnimResources());
+        ResourceLoader.get(PackType.CLIENT_RESOURCES).registerReloader(PlayerAnimResources.KEY, new PlayerAnimResources());
         if (FabricLoader.getInstance().isDevelopmentEnvironment() || FabricLoader.getInstance().getModContainer(PlayerAnimLibMod.MOD_ID).get().getMetadata().getVersion().getFriendlyString().contains("dev"))
             ClientCommandRegistrationCallback.EVENT.register(PlayerAnimCommands::register);
 
