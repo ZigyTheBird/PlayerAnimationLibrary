497fd41bb5c00343e533890aa06c5571497cdfad
Zigy <105124180+ZigyTheBird@users.noreply.github.com>
2025-09-25 16:29:24 +0330

    Update minecraft/common/src/main/java/com/zigythebird/playeranim/animation/PlayerAnimResources.java
    Co-authored-by: dima_dencep <dima_dencep@redlance.org>

--- minecraft/common/src/main/java/com/zigythebird/playeranim/animation/PlayerAnimResources.java
+++ minecraft/common/src/main/java/com/zigythebird/playeranim/animation/PlayerAnimResources.java
@@ -31,7 +31,7 @@ public class PlayerAnimResources implements ResourceManagerReloadListener {
 	 * @return animation, <code>null</code> if no animation
 	 */
 	public static @Nullable Animation getAnimation(ResourceLocation id) {
-		return ANIMATIONS.getOrDefault(id, null);
+		return ANIMATIONS.get(id);
 	}
 
 	/**
