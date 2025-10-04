88e6bd0e59fe7f4f55002f4247de496b25b95ac8
dima_dencep <dima_dencep@redlance.org>
2025-10-04 13:39:00 +0700

    Dev environment fixes and improvements (#44)
    Co-authored-by: Zigy <105124180+ZigyTheBird@users.noreply.github.com>

--- minecraft/common/src/main/java/com/zigythebird/playeranim/commands/AnimationArgumentProvider.java
+++ minecraft/common/src/main/java/com/zigythebird/playeranim/commands/AnimationArgumentProvider.java
@@ -5,7 +5,6 @@ import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.zigythebird.playeranim.animation.PlayerAnimResources;
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.SharedSuggestionProvider;
 import net.minecraft.resources.ResourceLocation;
 
@@ -13,13 +12,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-public class AnimationArgumentProvider implements SuggestionProvider<CommandSourceStack> {
+public class AnimationArgumentProvider<S> implements SuggestionProvider<S> {
     @Override
-    public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
         List<String> suggestions = new LinkedList<>();
         for (ResourceLocation animation : PlayerAnimResources.getAnimations().keySet()) {
             suggestions.add(animation.toString()); // TODO by names
         }
-        return SharedSuggestionProvider.suggest(suggestions.toArray(String[]::new), builder);
+        return SharedSuggestionProvider.suggest(suggestions, builder);
     }
 }
