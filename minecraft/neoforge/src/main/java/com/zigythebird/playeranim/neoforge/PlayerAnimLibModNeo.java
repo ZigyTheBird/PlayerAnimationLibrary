098bac164c490eb41db3f47d1a76c9c0ef94c215
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2025-11-20 18:56:49 +0330

    Move register events to correct thread on NeoForge

--- minecraft/neoforge/src/main/java/com/zigythebird/playeranim/neoforge/PlayerAnimLibModNeo.java
+++ minecraft/neoforge/src/main/java/com/zigythebird/playeranim/neoforge/PlayerAnimLibModNeo.java
@@ -6,31 +6,23 @@ import com.zigythebird.playeranim.api.PlayerAnimationAccess;
 import com.zigythebird.playeranim.commands.PlayerAnimCommands;
 import com.zigythebird.playeranim.neoforge.event.PlayerAnimationRegisterEvent;
 import com.zigythebird.playeranimcore.event.MolangEvent;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.neoforge.client.event.AddClientReloadListenersEvent;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import org.jetbrains.annotations.NotNull;
 
-@Mod(value = PlayerAnimLibModNeo.MOD_ID, dist = Dist.CLIENT)
+@Mod(value = PlayerAnimLibModNeo.MOD_ID)
 public final class PlayerAnimLibModNeo extends PlayerAnimLibMod {
     public PlayerAnimLibModNeo(IEventBus bus) {
         bus.addListener(this::onAddClientReloadListeners);
         if (!FMLLoader.getCurrent().isProduction() || ModList.get().getModFileById(PlayerAnimLibMod.MOD_ID).versionString().contains("dev"))
             NeoForge.EVENT_BUS.addListener(this::onRegisterCommands);
-
-        super.init();
-
-        PlayerAnimationAccess.REGISTER_ANIMATION_EVENT.register((player, manager) ->
-                NeoForge.EVENT_BUS.post(new PlayerAnimationRegisterEvent(player, manager))
-        );
-        MolangEvent.MOLANG_EVENT.register((controller, builder, q) ->
-                NeoForge.EVENT_BUS.post(new com.zigythebird.playeranim.neoforge.event.MolangEvent(controller, builder, q))
-        );
+        bus.addListener(this::onClientInit);
     }
 
     public void onAddClientReloadListeners(@NotNull AddClientReloadListenersEvent event) {
@@ -39,5 +31,18 @@ public final class PlayerAnimLibModNeo extends PlayerAnimLibMod {
 
     public void onRegisterCommands(RegisterClientCommandsEvent event) {
         PlayerAnimCommands.register(event.getDispatcher(), event.getBuildContext());
+    }
+
+    public void onClientInit(FMLClientSetupEvent event) {
+        event.enqueueWork(() -> {
+            super.init();
+
+            PlayerAnimationAccess.REGISTER_ANIMATION_EVENT.register((player, manager) ->
+                    NeoForge.EVENT_BUS.post(new PlayerAnimationRegisterEvent(player, manager))
+            );
+            MolangEvent.MOLANG_EVENT.register((controller, builder, q) ->
+                    NeoForge.EVENT_BUS.post(new com.zigythebird.playeranim.neoforge.event.MolangEvent(controller, builder, q))
+            );
+        });
     }
 }
